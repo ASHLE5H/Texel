d1ec744c8f3c1df544175a2e0256ac692d07b288
Ashlesh <ashlesh208@gmail.com>
2025-09-30 23:11:32 +0530

    fixed error during production

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,5 @@
 import logo1 from './logo1.png'
-import logo_icon from './logo_icon.png'
+import logo_icon from './logo_icon.PNG'
 import facebook_icon from './facebook_icon.svg'
 import instagram_icon from './instagram_icon.svg'
 import twitter_icon from './twitter_icon.svg'
